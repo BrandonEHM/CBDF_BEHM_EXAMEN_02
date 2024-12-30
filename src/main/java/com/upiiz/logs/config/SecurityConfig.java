9c90323fc062407cc6fce2c957421bb51148b8e7
BrandonEHM <brandonhermart@gmail.com>
2024-12-30 16:13:03 -0600

    Con CORS

--- src/main/java/com/upiiz/logs/config/SecurityConfig.java
+++ src/main/java/com/upiiz/logs/config/SecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -67,6 +70,19 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(admin, user, moderator, editor, developer, analyst);
     }
+
+    //CORS
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
